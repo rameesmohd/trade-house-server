dbc8ebeeb2cfc0a1224fd0149b5f708ba5d6ddc1
Rameez <rameesmohd789@gmail.com>
2023-10-10 16:05:58 +0530

    solved .env bugs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ connectDb()
 app.use(cors())
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
-
+console.log('api called.......');
 app.use('/admin',adminRouter)
 app.use('/tutor',tutorRouter)
 app.use('/',userRouter)
--- services/socket.js
+++ services/socket.js
@@ -4,7 +4,7 @@ const socket = (server) => {
     const io = require('socket.io')(server, {
         pingTimeout: 60000,
         cors: {
-            origin: "http://localhost:5173"
+            origin: process.env.FONTENDURL
         }
     })
     
