5c1d51ec20ed7064a5947e942caf9f22471344a5
Rameez <rameesmohd789@gmail.com>
2023-10-13 17:50:22 +0530

    changed axios to fetch in cron

--- services/cron.js
+++ services/cron.js
@@ -1,46 +1,58 @@
 const cron = require('node-cron');
-const axios = require('axios')
-const marketModel = require('../model/marketSchema')
+let fetch;
 
-const crone = ()=>{
-    cron.schedule('0 0 */3 * * *', () => {
-        console.log('This function runs every 3 days.')
-        const fetchcalenderData=async()=>{
-            const today = new Date(); 
-            const year = today.getFullYear();
-            const month = today.getMonth() + 1; 
-            const day = today.getDate();
-            const options = {
-            method: 'GET',
-            url: 'https://forex-factory-scraper1.p.rapidapi.com/get_real_time_calendar_details',
-            params: {
-              calendar: 'Forex',
-              year: year,
-              month: month,
-              day: day
-            },
-            headers: {
-              'X-RapidAPI-Key': '0f86848d76mshb648d21621dcc6bp1dd038jsn90030cf66930',
-              'X-RapidAPI-Host': 'forex-factory-scraper1.p.rapidapi.com'
-            }
-          };
-          
-          try {
-            const response = await axios.request(options);
-    
-            const economicCalenderData = response.data;
+import('node-fetch').then((module) => {
+  fetch = module.default;
+}).catch((error) => {
+  console.error('Error loading module:', error);
+});
 
-            console.log(economicCalenderData);
+const marketModel = require('../model/marketSchema');
 
-            await marketModel.updateOne({},{$set:{forexcalender:economicCalenderData}})
-            console.log('Economic calendar data saved to the database.');
-        } catch (error) {
-            console.error(error);
-        }    
+const crone = () => {
+  cron.schedule('0 0 */3 * * *', () => {
+    console.log('This function runs every 3 days.');
+    const fetchCalendarData = async () => {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = today.getMonth() + 1;
+      const day = today.getDate();
+      const url = 'https://forex-factory-scraper1.p.rapidapi.com/get_real_time_calendar_details';
+      const queryParams = new URLSearchParams({
+        calendar: 'Forex',
+        year: year,
+        month: month,
+        day: day
+      });
+
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '0f86848d76mshb648d21621dcc6bp1dd038jsn90030cf66930',
+          'X-RapidAPI-Host': 'forex-factory-scraper1.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(`${url}?${queryParams.toString()}`, options);
+
+        if (response.ok) {
+          const economicCalendarData = await response.json();
+          console.log(economicCalendarData);
+
+          // Assuming you have a 'marketModel' model for MongoDB
+          await marketModel.updateOne({}, { $set: { forexcalender: economicCalendarData } });
+          console.log('Economic calendar data saved to the database.');
+        } else {
+          console.error('Failed to fetch data. HTTP status:', response.status);
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
       }
-      fetchcalenderData()
-    })
-}
+    };
 
-module.exports = crone
+    fetchCalendarData();
+  });
+};
 
+module.exports = crone;
