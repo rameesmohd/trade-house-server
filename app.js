9b3973cbff9fafdc4a5a2c741567adfc20c26d6a
Rameez <rameesmohd789@gmail.com>
2023-10-13 16:05:03 +0530

    added cors

--- app.js
+++ app.js
@@ -23,7 +23,17 @@ const corsOptions = {
 	optionsSuccessStatus: 200,
   };
   
-app.use(cors())
+app.use(cors(corsOptions))
+
+app.use(cors({
+    origin: ["http://localhost:5173", "https://www.tradeh.online"],
+    methods: "GET,PUT,POST,DELETE,PATCH",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+    optionsSuccessStatus: 204 // Indicate that preflight requests should return a status code of 204
+  }));
+
+
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
 // cron()
