2fef861d4fb89968c59e18b9ddfe45f15dd7117a
Rameez <rameesmohd789@gmail.com>
2023-10-13 16:53:33 +0530

    configured cors

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use('/admin',adminRouter)
 app.use('/tutor',tutorRouter)
 app.use('/',userRouter)
 
-cron()
+
 const server = app.listen(process.env.PORT,()=>console.log("Server started at port",process.env.PORT))
 socket(server)
