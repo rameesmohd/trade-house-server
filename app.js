10dc6f936bd529532a11fecae2df6cf3dfc424cd
Rameez <rameesmohd789@gmail.com>
2023-10-13 17:25:25 +0530

    configured cors

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const tutorRouter = require('./routes/tutorRouter')
 const socket = require('./services/socket')
 const cron = require('./services/cron')
 connectDb()
-console.log('api called.......');
 
-// app.use(cors())  
-app.use(cors({credentials: true, origin: ['https://www.tradeh.online','http://www.tradeh.online']}));
-
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+  
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
 
