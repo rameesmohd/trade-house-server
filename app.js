e1cf609053f2ea470b4b073c69018841400fe052
Rameez <rameesmohd789@gmail.com>
2023-10-13 16:44:11 +0530

    configured cors

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ const cron = require('./services/cron')
 
 connectDb()
 
-app.use(cors({ origin: '*' }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+  
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
 console.log('api called.......');
