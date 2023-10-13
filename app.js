de70460e5bc3a3dba5df9567ee551072415ca3ed
Rameez <rameesmohd789@gmail.com>
2023-10-13 15:19:43 +0530

    added cors

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
 console.log('api called.......');
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+
 app.use('/admin',adminRouter)
 app.use('/tutor',tutorRouter)
 app.use('/',userRouter)
