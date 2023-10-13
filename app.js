6272537dc99373799feec1e85db57e71f72ca414
Rameez <rameesmohd789@gmail.com>
2023-10-13 15:38:30 +0530

    added cors

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const cron = require('./services/cron')
 
 connectDb()
 
-app.use(express.json({ limit: '3mb' }))
-app.use(express.urlencoded({ limit: '3mb', extended: true }))
-console.log('api called.......');
-
 const corsOptions = {
 	origin: [
 	  "http://localhost:5173",
@@ -29,6 +25,12 @@ const corsOptions = {
   
   // Use the cors middleware with the specified options
   app.use(cors(corsOptions));
+
+
+app.use(express.json({ limit: '3mb' }))
+app.use(express.urlencoded({ limit: '3mb', extended: true }))
+console.log('api called.......');
+
 
 
   
