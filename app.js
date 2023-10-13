985761efa7a2ff72e0fe245a3c1ee690f25eec42
Rameez <rameesmohd789@gmail.com>
2023-10-13 15:07:25 +0530

    added cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const socket = require('./services/socket')
 const cron = require('./services/cron')
 
 connectDb()
-app.use(cors())
+app.use(cors({ origin: process.env.FONTENDURL }))
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
 console.log('api called.......');
