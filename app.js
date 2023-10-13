c3256dc69bb4c050a019fda9e485f41c833e94cd
Rameez <rameesmohd789@gmail.com>
2023-10-13 16:28:47 +0530

    configured cors

--- app.js
+++ app.js
@@ -11,28 +11,15 @@ const cron = require('./services/cron')
 
 connectDb()
 
-app.use(cors({
-    origin: ["http://localhost:5173", "https://www.tradeh.online"],
-    methods: "GET,PUT,POST,DELETE,PATCH",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-    optionsSuccessStatus: 204 // Indicate that preflight requests should return a status code of 204
-  }));
-  
-  // Define a middleware specifically for handling preflight requests
-  app.options('*', (req, res) => {
-    res.status(204).send();
-  })
-
-
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
-// cron()
 console.log('api called.......');
-  
+
 app.use('/admin',adminRouter)
 app.use('/tutor',tutorRouter)
 app.use('/',userRouter)
 
+cron()
 const server = app.listen(process.env.PORT,()=>console.log("Server started at port",process.env.PORT))
 socket(server)
