ea7a31bde0636851d92c40f08b9ab5f464960836
Rameez <rameesmohd789@gmail.com>
2023-10-13 16:58:38 +0530

    configured cors

--- app.js
+++ app.js
@@ -8,19 +8,12 @@ const adminRouter = require('./routes/adminRouter')
 const tutorRouter = require('./routes/tutorRouter')
 const socket = require('./services/socket')
 const cron = require('./services/cron')
-
 connectDb()
+console.log('api called.......');
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
-  
+app.use(cors())
 app.use(express.json({ limit: '3mb' }))
 app.use(express.urlencoded({ limit: '3mb', extended: true }))
-console.log('api called.......');
 
 app.use('/admin',adminRouter)
 app.use('/tutor',tutorRouter)
